c70c4c2d391534c981281faf76e44d5c6c0be84f
luongngodac <55689857+luongngodac@users.noreply.github.com>
2020-04-24 23:56:11 +0700

    'lan_gan_cuoi'

--- app/src/main/java/vn/edu/ntu/ngodacluong/ngodacluong_59131342_simplewidget/MainActivity.java
+++ app/src/main/java/vn/edu/ntu/ngodacluong/ngodacluong_59131342_simplewidget/MainActivity.java
@@ -20,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
     TextView txtKQ;
     RadioButton rdbNam, rdbNu;
     CheckBox cbPhim, cbNhac, cbCafe, cbONha, cbNauAn;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,94 +50,57 @@ public class MainActivity extends AppCompatActivity {
         btXacNhan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
-                suKien1();
-                suKien2();
-                suKien3();
-                suKien4();
-                suKien5();
+                suKien();
             }
         });
-
     }
     //dinh nghia ham suKien
-    private void suKien1()
+    private void suKien()
     {
-        String editTen = edtTen.getText().toString().trim();
-        edtTen.setText(editTen);
-        Toast.makeText(getApplicationContext(), editTen , Toast.LENGTH_SHORT).show();
+        String textKQ = "";
+        textKQ = txtKQ.getText().toString();
+        textKQ = textKQ + edtTen.getText() + "\nNgày sinh: " + edtNgaySinh.getText() +"\nGiới tính: ";
+        if(rdgGioiTinh.getCheckedRadioButtonId() == R.id.rdbNam)
+        {
+            textKQ = textKQ + rdbNam.getText();
+        }
+        else
+        {
+            textKQ = textKQ + rdbNu.getText();
+        }
+        textKQ = textKQ + "\nSở thích: ";
+        if(cbPhim.isChecked())
+        {
+            textKQ = textKQ + cbPhim.getText() +"; ";
+        }
+        if(cbNhac.isChecked())
+        {
+            textKQ = textKQ + cbNhac.getText() +"; ";
+        }
+        if(cbCafe.isChecked())
+        {
+            textKQ = textKQ +cbCafe.getText() +"; ";
+        }
+        if(cbONha.isChecked())
+        {
+            textKQ = textKQ + cbONha.getText() +"; ";
+        }
+        if(cbNauAn.isChecked())
+        {
+            textKQ = textKQ + cbNauAn.getText() +"; ";
+        }
+        textKQ = textKQ + edtSTK.getText();
+
+
+
+        Toast.makeText(MainActivity.this, textKQ, Toast.LENGTH_LONG).show();
     }
-    private  void suKien2()
-    {
-        String editNgaySinh = edtNgaySinh.getText().toString().trim();
-        edtNgaySinh.setText(editNgaySinh);
-        Toast.makeText(getApplicationContext(), editNgaySinh, Toast.LENGTH_SHORT).show();
-    }
-    private void suKien3()
-    {
-        StringBuffer result = new StringBuffer();
-        result.append(rdbNam.isChecked());
-        result.append(rdbNu.isChecked());
-        Toast.makeText(getApplicationContext(), result.toString(),Toast.LENGTH_LONG).show();
-    }
-    private void suKien4()
-    {
-        StringBuffer resultcb = new StringBuffer();
-        resultcb.append(cbPhim.isChecked());
-        resultcb.append(cbNhac.isChecked());
-        resultcb.append(cbCafe.isChecked());
-        resultcb.append(cbONha.isChecked());
-        resultcb.append(cbNauAn.isChecked());
-        Toast.makeText(getApplicationContext(), resultcb.toString(), Toast.LENGTH_LONG).show();
-    }
-    private void suKien5()
-    {
-        String editSTK = edtSTK.getText().toString().trim();
-        edtSTK.setText(editSTK);
-        Toast.makeText(getApplicationContext(), editSTK, Toast.LENGTH_SHORT).show();
-    }
-
-    /** Called when the user touches the button */
-
-
-
-
-//    final CheckBox checkBox = findViewById(R.id.checkbox_id);
-////Kiểm tra checked
-//if (checkBox.isChecked())
-//    {
-//        //Checked
-//    }
-//else
-//    {
-//        //Unchecked
-//    }
-//
-//    //Thiết lập trạng thái check
-//    boolean toi_chon = true;
-//checkBox.setChecked(toi_chon);
-//
-////Bắt sự kiện thay đổi trạng thái
-//checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-//        @Override
-//        public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-//            //Code khi trạng thái check thay đổi
-//            Toast.makeText(
-//                    compoundButton.getContext(),
-//                    compoundButton.getText()+"|"+b,
-//                    Toast.LENGTH_SHORT).show();
-//        }
-//    });
-//
-////Bắt sự kiện Click
-//checkBox.setOnClickListener(new View.OnClickListener() {
-//        @Override
-//        public void onClick(View view) {
-//            Toast.makeText(
-//                    view.getContext(), "Click!",
-//                    Toast.LENGTH_SHORT).show();
-//        }
-//    });
-
 }
+//        sologan = String.valueOf(tvName.getText());
+//        if (cbMautim.isChecked()) {
+//            sologan = sologan + cbMautim.getText() + " - ";
+//        }
+//        if (cbMauhong.isChecked()) {
+//            sologan = sologan + cbMauhong.getText() + " - ";
+//        }
+//        Toast.makeText(MainActivity.this, sologan, Toast.LENGTH_LONG).show();
